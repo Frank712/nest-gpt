f2f9b0383433e55ee85f9c7f774bcf140977a5f5
Francisco Lino <franksuriel_7@hotmail.com>
2024-01-18 10:33:35 -0600

    UPDATE config open ai key

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  console.log('OPEN_API_KEY', process.env.OPEN_API_KEY);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();

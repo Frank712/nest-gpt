01af3f932e06ca0b851a549240931b70b8339770
Francisco Lino <franksuriel_7@hotmail.com>
2024-01-27 23:30:10 -0600

    ADD exports

--- src/gpt/use-cases/index.ts
+++ src/gpt/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from './orthography.use-case';
-export * from './pro-cons-discusser.use-case';
 export * from './pro-cons-discusser-stream.use-case';
+export * from './pro-cons-discusser.use-case';
+export * from './translate.use-case';
